b06e6add1e1218eb93b807a16b95edaecfd5078f
Alan Caruso <alan.carusoo@gmail.com>
2023-05-15 20:55:43 -0300

    changing task name

--- src/MainApp.js
+++ src/MainApp.js
@@ -6,17 +6,17 @@ export const MainApp = () => {
   const initialTask = [
     {
       id: Math.floor((1 + Math.random()) * 0x10000),
-      taskDescription: "test1",
+      taskDescription: "Task 1",
       status: 1,
     },
     {
       id: Math.floor((1 + Math.random()) * 0x10000),
-      taskDescription: "test2",
+      taskDescription: "Task 2",
       status: 1,
     },
     {
       id: Math.floor((1 + Math.random()) * 0x10000),
-      taskDescription: "test3",
+      taskDescription: "Task 3",
       status: 1,
     },
   ];
